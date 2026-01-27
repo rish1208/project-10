8fa37261c74562d71ae1a6839201ac0db0cfcb56
rish1208 <rishd01wivedi@gmail.com>
2026-01-27 14:59:16 +0530

    ok

---
+++ Project_10/src/main/java/com/rays/dao/RoleDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.RoleDTO;
+
+/**
+ * Role DAO interface.
+ *   Rishendra Dwivedi
+ */
+public interface RoleDAOInt extends BaseDAOInt<RoleDTO> {
+
+}
---
+++ Project_10/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ *   Rishendra Dwivedi
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
